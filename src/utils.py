6996ed0f48eab16d336a3e5af408d0802e6d4e4a
viplavs2004 <viplavs2004@gmail.com>
2025-07-29 18:00:25 +0530

    Home and Index page is completed

--- src/utils.py
+++ src/utils.py
@@ -38,7 +38,7 @@ def evaluate_models(x_train, y_train,x_test ,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
-            
+
            # model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
@@ -55,4 +55,12 @@ def evaluate_models(x_train, y_train,x_test ,y_test,models,param):
     except Exception as e:
          raise CustomException(e,sys)
     
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+              
+    except Exception as e:
+        raise CustomException(e,sys)
+               
